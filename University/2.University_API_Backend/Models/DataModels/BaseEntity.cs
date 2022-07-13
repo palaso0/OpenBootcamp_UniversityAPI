1fc35cadba930040872e6e124bfb4dcf19196493
Paulo Choque <Paulo.Choque@fundacion-jala.org>
2022-07-12 20:08:35 -0400

    task1 Course Added

---
+++ University/2.University_API_Backend/Models/DataModels/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _2.University_API_Backend.Models.DataModels
+{
+    public class BaseEntity
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime? UpdatedAt { get; set; }
+        public string DeletedBy { get; set; } = string.Empty;
+        public DateTime? DeletedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        //Estos son campos que estarán presentes en la DB
+        public BaseEntity()
+        {
+            
+        }
+    }
+}
+
